93f2bbeca7c58616b3bfaaf50614bb2ac5e48ac7
MuzykaAndrii <andrymyzik@gmail.com>
2024-04-22 22:43:21 +0300

    impl: exams api endpoint

--- app/main/urls.py
+++ app/main/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from main.views import ExamRegisterView
+from main.views import ExamRegisterView, ExamsEndpoint
 
 
 app_name = "exams"
@@ -8,4 +8,5 @@ app_name = "exams"
 
 urlpatterns = [
     path("register/", ExamRegisterView.as_view(), name="register"),
+    path("api/", ExamsEndpoint.as_view(), name="api"),
 ]
--- app/main/views.py
+++ app/main/views.py
@@ -2,6 +2,13 @@ from django.http import HttpRequest
 from django.shortcuts import render
 from django.views import View
 
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import status
+
+from main.models import Degree
+from main.serializers import DegreeSerializer
+
 
 class ExamRegisterView(View):
     def get(self, request: HttpRequest):
@@ -10,3 +17,12 @@ class ExamRegisterView(View):
     def post(self, request: HttpRequest):
         # handle form
         pass
+
+
+class ExamsEndpoint(APIView):
+    def get(self, request: HttpRequest):
+        degrees = Degree.objects.all()
+
+        serializer = DegreeSerializer(degrees, many=True)
+
+        return Response(serializer.data)
