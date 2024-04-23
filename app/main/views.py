afe2a27471e7b4e3961782d8338b58836dd3d7a0
MuzykaAndrii <andrymyzik@gmail.com>
2024-04-24 00:11:17 +0300

    impl: consistent exam data json serialization

--- app/main/views.py
+++ app/main/views.py
@@ -1,10 +1,10 @@
 from django.http import HttpRequest
 from django.shortcuts import render
 from django.views import View
+import json
 
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework import status
 
 from main.models import Degree
 from main.serializers import DegreeSerializer
@@ -14,8 +14,9 @@ class ExamRegisterView(View):
     def get(self, request: HttpRequest):
         degrees = Degree.objects.all()
         serializer = DegreeSerializer(degrees, many=True)
+        data = json.dumps(serializer.data)
     
-        return render(request, "main/register.html", context={"data": serializer.data})
+        return render(request, "main/register.html", context={"data": data})
 
     def post(self, request: HttpRequest):
         # handle form
