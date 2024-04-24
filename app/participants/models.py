0ab519fc798f1424b59b960a169581b012fa0fcb
MuzykaAndrii <andrymyzik@gmail.com>
2024-04-24 17:25:41 +0300

    impl: participant model

--- app/participants/models.py
+++ app/participants/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+
+class Participant(models.Model):
+    firstname = models.CharField(
+        verbose_name="Імя",
+        max_length=50,
+        null=False,
+        blank=False,
+    )
+    lastname = models.CharField(
+        verbose_name="Прізвище",
+        max_length=50,
+        null=False,
+        blank=False,
+    )
+    surname = models.CharField(
+        verbose_name="По батькові",
+        max_length=50,
+        null=False,
+        blank=False,
+    )
+    email = models.EmailField(
+        verbose_name="Електронна пошта",
+        null=False,
+        blank=False,
+    )
+    phone = models.CharField(
+        verbose_name="Номер телефону",
+        max_length=20,
+        null=True,
+        blank=True,
+    )
